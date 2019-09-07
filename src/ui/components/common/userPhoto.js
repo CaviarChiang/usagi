117cf3308a2c34be4f920d77aa7237a2ecb1d74c
CaviarChiang <yuyanj143@foxmail.com>
2019-09-07 01:42:31 -0400

    Changed spaces to tabs

--- src/ui/components/common/userPhoto.js
+++ src/ui/components/common/userPhoto.js
@@ -4,34 +4,34 @@ import { withStyles } from '@material-ui/core/styles';
 import Avatar from '@material-ui/core/Avatar';
 
 const styles = ({
-  profilePhoto: {
-    position: 'relative',
-  },
-  avatar: {
-    margin: 0,
-    width: 60,
-    height: 60,
-  },
+	profilePhoto: {
+		position: 'relative',
+	},
+	avatar: {
+		margin: 0,
+		width: 60,
+		height: 60,
+	},
 });
 
 class UserPhoto extends React.Component {
-  render() {
-    const { user, classes } = this.props;
+	render() {
+		const { user, classes } = this.props;
 
-    return (
-      <div className={classes.profilePhoto}>
-        <Avatar
-          src={user.photo}
-          className={`avatar ${classes.avatar}`}
-        />
-      </div>
-    );
-  }
+		return (
+			<div className={classes.profilePhoto}>
+				<Avatar
+					src={user.photo}
+					className={`avatar ${classes.avatar}`}
+				/>
+			</div>
+		);
+    }
 }
 
 UserPhoto.propTypes = {
-  classes: PropTypes.object.isRequired,
-  user: PropTypes.object.isRequired,
+	classes: PropTypes.object.isRequired,
+	user: PropTypes.object.isRequired,
 };
 
 export default withStyles(styles)(UserPhoto);
