4f12a44056ca9021b79709053dc8c93741f08805
CaviarChiang <yuyanj143@foxmail.com>
2019-09-07 01:39:17 -0400

    Added chats list

--- src/ui/app/chatListPage.js
+++ src/ui/app/chatListPage.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Moment from 'react-moment';
+import { withStyles } from '@material-ui/core/styles';
+import ListItem from '@material-ui/core/ListItem/ListItem';
+import Snackbar from '@material-ui/core/Snackbar';
 
 import UserPhoto from '../components/common/userPhoto';
-
-const propTypes = {
-	input: PropTypes.string,
-};
-
-const defaultProps = {
-};
 
 const kyabia = {
 	"id": 11198,
@@ -22,15 +19,76 @@ const kyle = {
 	"name": "Zyle Khou",
 }
 
+const chats = [
+	{
+		id: 7956,
+		user: kyle,
+		lastMessage: {
+			text: "I miss you",
+			timestamp: "2019-09-05T10:17:09",
+		},
+	},
+	{
+		id: 6123,
+		user: kyabia,
+		lastMessage: {
+			text: "I miss you too!",
+			timestamp: "2019-09-05T10:19:11",
+		},
+	},
+]
+
+const styles = ({
+	icon: {
+		fontSize: 14,
+		margin: 0,
+	},
+	avatar: {
+		margin: 0,
+		width: 60,
+		height: 60,
+	},
+});
+
+const propTypes = {
+	input: PropTypes.string,
+};
+
+const defaultProps = {};
+
 class ChatListPage extends React.Component {
-	constructor(props) {
-		super(props);
-	}
 	render() {
 		return <div className='kyabia :)'>
 			<p>this is your space {this.props.input}</p>
 			<UserPhoto user={kyabia}></UserPhoto>
 			<UserPhoto user={kyle}></UserPhoto>
+
+			{chats.map((chat, index) => (
+				<ListItem button className="user" key={index}>
+					<UserPhoto user={chat.user} />
+					<div className="details">
+						<p className="name">{chat.user.name}</p>
+						<p className="lastMessage">{chat.lastMessage.text}</p>
+					</div>
+					<div className="status">
+						<Moment date={chat.lastMessage.timestamp} />
+					</div>
+
+					<Snackbar
+					anchorOrigin={{
+						vertical: 'bottom',
+						horizontal: 'left',
+					}}
+					autoHideDuration={1000}
+					open={false}
+					ContentProps={{
+						'aria-describedby': 'message-id',
+					}}
+					message={<span id="message-id">{chat.user.name} online</span>}
+					/>
+				</ListItem>
+			))}
+		
 		</div>
 	}
 }
@@ -38,4 +96,4 @@ class ChatListPage extends React.Component {
 ChatListPage.propTypes = propTypes;
 ChatListPage.defaultProps = defaultProps;
 
-export default ChatListPage;
+export default withStyles(styles)(ChatListPage);
